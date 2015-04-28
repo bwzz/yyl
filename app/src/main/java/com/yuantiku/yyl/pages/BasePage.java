b392e5547e2f955ca06ae5579f91e37ce54ba035
wanghb <wanghb@fenbi.com>
2015-04-28 19:59:25 +0800

    resolve conflict

---
+++ app/src/main/java/com/yuantiku/yyl/pages/BasePage.java
@@ -0,0 +1,10 @@
+package com.yuantiku.yyl.pages;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author wanghb
+ * @date 15/4/25.
+ */
+public class BasePage extends Fragment {
+}
---
+++ app/src/main/java/com/yuantiku/yyl/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.yuantiku.yyl.pages;
+
+/**
+ * @author wanghb
+ * @date 15/4/25.
+ */
+public class LoginPage extends BasePage {
+}
