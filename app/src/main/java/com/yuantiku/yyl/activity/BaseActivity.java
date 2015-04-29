f48cd5b2c09f7564de6dab97cfcbfd1ca7cd006d
lirui <lirui@fenbi.com>
2015-04-29 12:01:19 +0800

    MOD: move MainActivity to activity package

--- app/src/main/java/com/yuantiku/yyl/activity/BaseActivity.java
+++ app/src/main/java/com/yuantiku/yyl/activity/BaseActivity.java
@@ -2,6 +2,7 @@ package com.yuantiku.yyl.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import com.yuantiku.yyl.util.LogUtils;
 
@@ -11,11 +12,11 @@ import butterknife.ButterKnife;
  * @author lirui
  * @date 15/4/13.
  */
-public abstract class BaseActivity extends FragmentActivity {
+public abstract class BaseActivity extends ActionBarActivity {
 
     public static LogUtils L;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
         ButterKnife.inject(this);
--- app/src/main/java/com/yuantiku/yyl/activity/MainActivity.java
+++ app/src/main/java/com/yuantiku/yyl/activity/MainActivity.java
@@ -1,20 +1,27 @@
 package com.yuantiku.yyl.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.yuantiku.yyl.R;
-import com.yuantiku.yyl.webadapter.WikiAdapter;
-import com.yuantiku.yyl.interfaces.WikiService;
+import com.yuantiku.yyl.activity.BaseActivity;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-
-/**
- * @author lirui
- * @date 15/4/26.
- */
 public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
 
     @Override
     protected int getLayoutResId() {
@@ -22,19 +29,39 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        WikiService wikiService = WikiAdapter.getService();
-        wikiService.login("lirui", "", "Login", new Callback<String>() {
-            @Override
-            public void success(String s, Response response) {
-                L.i("success");
-            }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-            @Override
-            public void failure(RetrofitError error) {
-                L.e(error.getMessage());
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
