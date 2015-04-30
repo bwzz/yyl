b4ced043a2b219fba5c31436b0d2f1f90aae0bb5
lrhehe <lrhehe@gmail.com>
2015-04-30 17:20:01 +0800

    ADD: add persistent cookie store

--- app/src/main/java/com/yuantiku/yyl/MyApplication.java
+++ app/src/main/java/com/yuantiku/yyl/MyApplication.java
@@ -3,6 +3,7 @@ package com.yuantiku.yyl;
 import android.app.Application;
 
 import com.yuantiku.yyl.util.CrashHandler;
+import com.yuantiku.yyl.util.PersistentCookieStore;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
@@ -15,7 +16,7 @@ import java.net.CookiePolicy;
 public class MyApplication extends Application {
     private static MyApplication application;
 
-    public static MyApplication getInstance(){
+    public static MyApplication getInstance() {
         return application;
     }
 
@@ -24,7 +25,8 @@ public class MyApplication extends Application {
         super.onCreate();
         application = this;
         CrashHandler.getInstance().init(this);
-        CookieManager cookieManager = new CookieManager();
+        CookieManager cookieManager = new CookieManager(PersistentCookieStore.getInstance(this),
+                CookiePolicy.ACCEPT_ALL);
         cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
         CookieHandler.setDefault(cookieManager);
     }
