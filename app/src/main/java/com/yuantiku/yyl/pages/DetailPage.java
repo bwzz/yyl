96c2dfd431a2297d638573789ff5b1b44d7f5944
lrhehe <lrhehe@gmail.com>
2015-05-08 11:39:35 +0800

    MOD: change icon and add email action

--- app/src/main/java/com/yuantiku/yyl/pages/DetailPage.java
+++ app/src/main/java/com/yuantiku/yyl/pages/DetailPage.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -33,8 +34,8 @@ public class DetailPage extends FragmentPage {
     @InjectView(R.id.birthday)
     TextView birthday;
 
-    @InjectView(R.id.constellation)
-    TextView constellation;
+    @InjectView(R.id.googleAccount)
+    TextView googleAccount;
 
     @InjectView(R.id.constellationName)
     TextView constellationName;
@@ -53,7 +54,7 @@ public class DetailPage extends FragmentPage {
         phone.setText(contact.getPhone());
         email.setText(contact.getEmail());
         birthday.setText(contact.getBirth());
-        constellation.setText(contact.getConstellation());
+        googleAccount.setText(contact.getGoogleAccount());
 
         // TODO
         constellationName.setText(contact.getConstellation());
@@ -66,14 +67,36 @@ public class DetailPage extends FragmentPage {
         return super.setupView(view);
     }
 
-    @OnClick(R.id.phone)
-    public void call(View view) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel://" + contact.getPhone()));
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            // TODO
+    @OnClick({R.id.phone, R.id.email, R.id.googleAccount})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.phone:
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel://" + contact.getPhone()));
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    // TODO
+                }
+                break;
+            case R.id.email:
+                emailTo(contact.getEmail());
+                break;
+            case R.id.googleAccount:
+                emailTo(contact.getGoogleAccount());
+                break;
+
         }
+    }
+
+    private void emailTo(String email) {
+        if (TextUtils.isEmpty(email)) {
+            return;
+        }
+        Intent data = new Intent(Intent.ACTION_SENDTO);
+        data.setData(Uri.parse("mailto:" + email));
+        data.putExtra(Intent.EXTRA_SUBJECT, "");
+        data.putExtra(Intent.EXTRA_TEXT, "");
+        startActivity(data);
     }
 }
